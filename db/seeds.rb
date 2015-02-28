a2e18b90a862deef29b800e94cfa6104cf1aa0c6
sh.johnson <sh.johnson@me.com>
2015-02-28 20:05:35 +0000

    Altered seeds file to include recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,24 @@
 current_users = User.all.count
-10.times { User.create(Fabricate.attributes_for(:user)) }
+current_recipes = Recipe.all.count
+
+## <--------Users without any recipes---------->
+
+2.times { User.create(Fabricate.attributes_for(:user)) }
+
+## <--------Recipes with a user---------->
+
+3.times { Recipe.create(Fabricate.attributes_for(:recipe)) }
+
+## <--------User with 5 recipes---------->
+user = User.create(Fabricate.attributes_for(:user))
+5.times { Recipe.create(Fabricate.attributes_for(:recipe, user: user)) }
+
+
+
+created_recipes = Recipe.all.count - current_recipes
 created_users = User.all.count - current_users
-p "#{created_users} users created with these names: #{User.last(10).map(&:username)}"
+
+p "#{created_users} users created with these usernames: #{User.last(10).map(&:username)}"
 p "Total users in the database = #{User.count}"
+p "#{created_recipes} recipes created"
+p "This user has 5 recipes :#{user.username}"
