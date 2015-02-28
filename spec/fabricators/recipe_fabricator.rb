5da8f779ac2ac1ef61f1ae62b9ac8de236a5451b
sh.johnson <sh.johnson@me.com>
2015-02-28 19:35:47 +0000

    add recipe fabricator

---
+++ spec/fabricators/recipe_fabricator.rb
@@ -0,0 +1,9 @@
+recipes = ['goats cheese tart', 'fish pie', 'fish and chips', 'strawberries and cream', 'plum tart', 'steak and ale pie', 'chicken and rice', 'pealla with fish', 'steak with peas', 'prawns in pasta']
+
+Fabricator(:recipe) do
+  id { SecureRandom.uuid }
+  user
+  name { recipes.sample }
+  rating { [1, 2, 3, 4, 5].sample }
+  user_id { |attrs| attrs[:user].id }
+end
